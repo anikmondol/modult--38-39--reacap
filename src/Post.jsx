5ae6068d73828fab5b56d56f12bb7b1119babfd6
anikmondol <anikmondol558363@gmil.com>
2023-10-18 15:13:10 +0600

    test 1

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import './post.css'
+import './Post.css';
 
 const Post = ({post}) => {
     const {title,userId,body,id} = post;
     return (
-        <div className='post'>
+        <div className='Post'>
             <h4>Title: {title}</h4>
             <p><small>userId: {userId}</small></p>
             <p><small>postId: {id}</small></p>
